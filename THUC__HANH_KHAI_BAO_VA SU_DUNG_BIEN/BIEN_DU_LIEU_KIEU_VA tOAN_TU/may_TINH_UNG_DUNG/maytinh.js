5a408045c371289349f2e04750a7b54037765316
vovantai1999 <108655530+vovantai1999@users.noreply.github.com>
2022-07-13 19:55:17 +0700

    akakakakaka

---
+++ THUC__HANH_KHAI_BAO_VA SU_DUNG_BIEN/BIEN_DU_LIEU_KIEU_VA tOAN_TU/may_TINH_UNG_DUNG/maytinh.js
@@ -0,0 +1,15 @@
+function myFunction(){
+    let number1 = document.getElementById;('number1'). value;
+    let number2 = document.getElementById;('number2').value;
+    let selec1 = document.getElementById;('selec1'). value;
+    let result = 0;
+    if(selec1 == "+"){
+        result = number(number1) + number(number2);
+        }else if(selec1 =="-"){
+        result = number(number1) - number(number2);
+        }
+        document.getElementById('result'). value = result;
+}
+    
+
+
