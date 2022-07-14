aa081865a0a77400e3eee4df21b5879e1be6675b
vovantai1999 <108655530+vovantai1999@users.noreply.github.com>
2022-07-14 10:29:08 +0700

    tính năm nhuận

--- THUC__HANH_KHAI_BAO_VA SU_DUNG_BIEN/BIEN_DU_LIEU_KIEU_VA tOAN_TU/may_TINH_UNG_DUNG/maytinh.js
+++ THUC__HANH_KHAI_BAO_VA SU_DUNG_BIEN/BIEN_DU_LIEU_KIEU_VA tOAN_TU/may_TINH_UNG_DUNG/maytinh.js
@@ -1,15 +1,17 @@
 function myFunction(){
-    let number1 = document.getElementById;('number1'). value;
-    let number2 = document.getElementById;('number2').value;
-    let selec1 = document.getElementById;('selec1'). value;
-    let result = 0;
-    if(selec1 == "+"){
-        result = number(number1) + number(number2);
-        }else if(selec1 =="-"){
-        result = number(number1) - number(number2);
-        }
-        document.getElementById('result'). value = result;
+    let number1 = document.getElementById("number1").value;
+    let number2 = document.getElementById("number2").value;
+    let operator = document.getElementById("operator").value;
+    var result = 0;
+    if(operator=="+"){
+        result = parseInt(number1) + parseInt(number2);
+    }else if(operator=="-"){
+        result = parseInt(number1) - parseInt(number2);
+    }else if(operator=="*"){
+        result = parseInt(number1) * parseInt(number2);
+    }else{
+        result = parseInt(number1) / parseInt(number2);
+        document.getElementById("result").value = result;  
+    }
 }
-    
-
 
