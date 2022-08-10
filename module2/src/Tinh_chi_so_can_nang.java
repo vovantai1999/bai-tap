234fb364ae01e5c55733c9df96c8533045f95d92
vovantai1999 <108655530+vovantai1999@users.noreply.github.com>
2022-08-10 14:04:20 +0700

    tính chỉ số cân nặng

---
+++ module2/src/Tinh_chi_so_can_nang.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Tinh_chi_so_can_nang {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double weight, height, bmi;
+        System.out.println("Nhập cân nặng(kg)");
+        weight = scanner.nextDouble();
+        System.out.println("Nhập chiều cao(mét)");
+        height = scanner.nextDouble();
+
+        bmi = weight / Math.pow(height, 2);
+        System.out.println("Chỉ số cân nặng cơ thể là: " + bmi );
+        if (bmi <= 18.5){
+            System.out.println(" Thiếu cân nặng");
+        } else if (bmi < 25.0) {
+            System.out.println("Bình thường");
+        } else if (bmi < 30.0) {
+            System.out.println("Thừa cân nặng");
+        }else {
+            System.out.println("Béo phì");
+        }
+    }
+}
