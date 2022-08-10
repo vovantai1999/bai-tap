017fd923b4bca61a31112f626aa965e188bb5a79
vovantai1999 <108655530+vovantai1999@users.noreply.github.com>
2022-08-10 09:16:13 +0700

    bài tập

---
+++ module2/src/Hien_Thi_Thoi_Gian_Hien_Tai.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Hien_Thi_Thoi_Gian_Hien_Tai {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+}
