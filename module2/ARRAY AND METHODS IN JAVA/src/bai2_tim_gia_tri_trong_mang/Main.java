4b91b39bff894dc341689309720adb2013d382dd
vovantai1999 <108655530+vovantai1999@users.noreply.github.com>
2022-08-12 09:13:34 +0700

    tìm vị trí tring mảng

---
+++ module2/ARRAY AND METHODS IN JAVA/src/bai2_tim_gia_tri_trong_mang/Main.java
@@ -0,0 +1,4 @@
+package bai2_tim_gia_tri_trong_mang;
+
+public class Main {
+}
---
+++ module2/ARRAY AND METHODS IN JAVA/src/bai2_tim_gia_tri_trong_mang/Tim_gia_tri_trong_mang.java
@@ -0,0 +1,23 @@
+package bai2_tim_gia_tri_trong_mang;
+
+import java.util.Scanner;
+
+public class Tim_gia_tri_trong_mang {
+    public static void main(String[] args) {
+        String[] students = {"Tai", "Tung", "Hieu", "Khanh", "Binh", "Quy"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập tên một học sinh: ");
+        String input_name = scanner.nextLine();
+        Boolean isExist = false;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].equals(input_name)) {
+                System.out.println("Vị trí học sinh: " + input_name + " là:" + i);
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist) {
+            System.out.println("Không tìm thấy " + input_name + " trong danh sách");
+        }
+    }
+}
