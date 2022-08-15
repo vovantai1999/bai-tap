2dc8d15aca7b4b636f4adb61d17037c37e1b3904
vovantai1999 <108655530+vovantai1999@users.noreply.github.com>
2022-08-15 14:43:43 +0700

    selextion Sort

--- module2/SORT/src/bai3_cai_dat_thuat_toan_sap_xep_chon/Main.java
+++ module2/SORT/src/bai3_cai_dat_thuat_toan_sap_xep_chon/Main.java
@@ -1,30 +1,5 @@
 package bai3_cai_dat_thuat_toan_sap_xep_chon;
 
 public class Main {
-    static double[] list = {2, 6.6, 9, 5.5, 13, 7.3, 44, 3.3};
 
-    public static void selectionSort(double[] list) {
-        for (int i = 0; i < list.length - 1; i++) {
-            double currentMin = list[i];
-            int curentMinIndex = i;
-
-            int currentMinIndex =  0;
-            for (int j = i + 1; j < list.length; j++) {
-                if (currentMin > list[j]) {
-                    currentMin = list[j];
-                    currentMinIndex = j;
-                }
-            }
-            if (currentMinIndex != i) {
-                list[currentMinIndex] = list[i];
-                list[i] = currentMin;
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        selectionSort(list);
-        for (int i = 0; i < list.length; i++)
-            System.out.print(list[i] + " ");
-    }
 }
