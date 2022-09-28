952b811112b2fa509fb04ddc71af94f96dc88e25
vovantai1999 <108655530+vovantai1999@users.noreply.github.com>
2022-09-28 18:44:51 +0700

    các hàm thông dụng

--- module3/CÁC HÀM THÔNG DỤNG TRONG SQL/TH sử dụng hàm thông dụng trong SQL.sql
+++ module3/CÁC HÀM THÔNG DỤNG TRONG SQL/TH sử dụng hàm thông dụng trong SQL.sql
@@ -20,3 +20,20 @@ SELECT S.StudentId, S.StudentName, AVG(Mark) FROM Student S join Mark M on S.Stu
 GROUP BY S.StudentId, S.StudentName;
 SELECT S.StudentId, S.StudentName, AVG(Mark) FROM Student S join Mark M on S.StudentId = M.StudentId GROUP BY S.StudentId, S.StudentName
 HAVING AVG(Mark) >= ALL (SELECT AVG(Mark) FROM Mark GROUP BY Mark.StudentId);
+
+-- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
+USE quanlysinhvien;
+SELECT MAX(Credit) FROM subject;
+
+-- Hiển thị các thông tin môn học có điểm thi lớn nhất.
+SELECT * FROM subject 
+WHERE subID = (
+SELECT subID FROM mark 
+WHERE mark = (SELECT MAX(mark) FROM mark)
+);
+
+ -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần.
+ SELECT *, AVG(m.mark) FROM student s 
+INNER JOIN mark m ON s.studentID = m.studentID
+GROUP BY s.studentID
+ORDER BY AVG(m.mark) DESC;
